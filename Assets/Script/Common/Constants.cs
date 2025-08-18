dc955a3c539acdc741ec6719106b2154a06ac11e
ToastBox <car9245@naver.com>
2025-08-18 17:06:19 +0900

    [Resource] 버튼 클릭 이미지 적용 및 기능 중간 머지

--- Assets/Script/Common/Constants.cs
+++ Assets/Script/Common/Constants.cs
@@ -11,6 +11,19 @@ public class Constants
     public const string TitleSceneName = "Title";
     public const string GameSceneName = "InGame";
     #endregion
+
+    #region ButtonSprites
+
+    public const string SprAcceptUp = "AP_up";
+    public const string SprAcceptDown = "AP_down";
+
+    public const string SprNegativeUp = "DE_up";
+    public const string SprNegativeDown = "DE_down";
+
+    public const string SprPauseUp = "Pause_up";
+    public const string SprPauseDown = "Pause_down";
+
+    #endregion
     
     //
     public enum GameState
