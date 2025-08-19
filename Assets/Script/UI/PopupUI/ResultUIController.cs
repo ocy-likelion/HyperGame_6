479a04dad3f7b779c91b4ea586ba23cb8ef2b8e6
Mobics <woqja0307@naver.com>
2025-08-19 15:55:16 +0900

    [Feat] Add Result Effect
    - 게임 결과 표시 효과 제작 (점수가 순차적으로 0부터 오르도록)
    - 만약 결과값이 신기록이라면 추가 효과 제작 (New Record!! 글자가 생성되며 계속 깜빡거리도록)

--- Assets/Script/UI/PopupUI/ResultUIController.cs
+++ Assets/Script/UI/PopupUI/ResultUIController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ResultUIController : PopupController
 {
@@ -14,9 +15,14 @@ public class ResultUIController : PopupController
     [SerializeField] private TMP_Text maxComboText;
     [SerializeField] private TMP_Text scoreText;
 
+    [Header("New Record UI")]
+    [SerializeField] private TMP_Text newRecordText;
+
     void Awake()
     {
         _quitButton.onClick.AddListener(OnClickQuitButton);
+        if (newRecordText != null)
+            newRecordText.gameObject.SetActive(false);
     }
     
     public void ShowPopup()
@@ -31,9 +37,60 @@ public class ResultUIController : PopupController
     
     public void InitResultItem(GameResultData resultData)
     {
-        dayText.text = resultData.Day.ToString();
-        maxComboText.text = resultData.MaxCombo.ToString();
-        scoreText.text = resultData.Score.ToString();
+        // 처음에는 0으로 초기화
+        dayText.text = "0";
+        maxComboText.text = "0";
+        scoreText.text = "0";
+        
+        // TODO: 유저의 최고기록 불러오기 (임시: PlayerPrefs)
+        int bestScore = PlayerPrefs.GetInt("BestScore", 0);
+        
+        Sequence seq = DOTween.Sequence();
+        
+        // Day Count Up
+        seq.Append(DOTween.To(() => 0, x => dayText.text = x.ToString() + "일", resultData.Day, 1f));
+        seq.AppendInterval(0.2f);
+        
+        // MaxCombo Count Up
+        seq.Append(DOTween.To(() => 0, x => maxComboText.text = x.ToString(), resultData.MaxCombo, 1f));
+        seq.AppendInterval(0.2f);
+
+        // Score Count Up
+        seq.Append(DOTween.To(() => 0, x => scoreText.text = x.ToString(), resultData.Score, 1.5f)
+            .OnComplete(() =>
+            {
+                // New Record 체크
+                if (resultData.Score > bestScore)
+                {
+                    PlayerPrefs.SetInt("BestScore", resultData.Score);
+                    ShowNewRecordEffect();
+                }
+            }));
+    }
+
+    // New Record 시, 효과
+    public void ShowNewRecordEffect()
+    {
+        if (newRecordText != null)
+        {
+            newRecordText.gameObject.SetActive(true);
+            newRecordText.alpha = 0f;
+            newRecordText.transform.localScale = Vector3.zero * 0.8f;
+            
+            Sequence seq = DOTween.Sequence();
+            // Fade In
+            seq.Append(newRecordText.DOFade(1f, 0.5f));
+            // 글자가 살짝 커졌다가 원래 크기로
+            seq.Join(newRecordText.transform.DOScale(1.2f, 0.3f).SetEase(Ease.OutBack));
+            seq.Append(newRecordText.transform.DOScale(1f, 0.2f).SetEase(Ease.InOutSine));
+            // 번쩍거리는 효과 무한 반복
+            seq.OnComplete(() =>
+            {
+                newRecordText.DOFade(0.3f, 0.2f)
+                    .SetLoops(-1, LoopType.Yoyo)
+                    .SetEase(Ease.InOutSine);
+            });
+        }
     }
 
     public void OnClickQuitButton()
