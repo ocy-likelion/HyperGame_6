462ff8960a2fda3e2dafc44334ffd341859da69e
hoon <simhoon0905@naver.com>
2025-08-26 15:32:56 +0900

    InfoUIController PopupController 상속

--- Assets/Script/UI/PopupUI/InfoUIController.cs
+++ Assets/Script/UI/PopupUI/InfoUIController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InfoUIController : MonoBehaviour
+public class InfoUIController : PopupController
 {
     Button touchButton;
     Image touchImage;
