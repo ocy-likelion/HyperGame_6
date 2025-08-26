8f84fb02c079b8f29a1e6202883d146794a0d713
KwonYJ <66343178+psyche1021@users.noreply.github.com>
2025-08-26 15:17:43 +0900

    [Refactor] 메인 화면 로고 애니메이션 코드 수정

--- Assets/@KYJ/MainLogoAnim.cs
+++ Assets/@KYJ/MainLogoAnim.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -9,12 +10,9 @@ public class NewBehaviourScript : MonoBehaviour
 
     void Start()
     {
-        logo.localScale = Vector3.zero;
-        logo.DOScale(1f, 1f).SetEase(Ease.OutBack);
-
-        var image = logo.GetComponent<UnityEngine.UI.Image>();
-        image.DOFade(0f, 0f).From().SetEase(Ease.Linear);
-
+        var image = logo.GetComponent<Image>();
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
+        logo.GetComponent<Image>().DOFade(1f, 0.5f).SetEase(Ease.Linear);
         logo.DOLocalMoveY(logo.localPosition.y + 25f, 1.5f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
     }
 }
