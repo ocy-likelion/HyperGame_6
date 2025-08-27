1d398487cf99891f97702c5169a7223091c3f581
조재현 <rattenfanger44@gmail.com>
2025-08-27 14:56:43 +0900

    [Fix] 카운트다운 리소스 적용, 타이틀 로고 pivot 수정

--- Assets/Script/UI/InGameUI/WaitThreeSecondsUI.cs
+++ Assets/Script/UI/InGameUI/WaitThreeSecondsUI.cs
@@ -1,30 +1,40 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WaitThreeSecondsUI : MonoBehaviour
 {
-    public TMP_Text SecondsText; // UI에 표시할 텍스트 컴포넌트
-    public Image TouchBlockPanel; // UI에 표시할 텍스트 컴포넌트
+    public Image CountdownImage; // 카운트다운 이미지 UI
+    public Sprite[] CountdownSprites; // [0] = 3, [1] = 2, [2] = 1, [3] = Start
+    public Image TouchBlockPanel; // 터치 막는 패널
 
     public IEnumerator WaitThreeSeconds()
     {
-        SecondsText.gameObject.SetActive(true); // 텍스트 활성화
-        TouchBlockPanel.gameObject.SetActive(true); //터치 막는 패널 활성화
-        Time.timeScale = 0f; // 게임 시간 정지
-        SecondsText.text = "3"; // 3초 표시
-        AudioManager.Instance.SFX.PlayGameStart(); // 게임 시작 사운드 재생
+        CountdownImage.gameObject.SetActive(true);
+        TouchBlockPanel.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+
+        if (CountdownSprites.Length < 4)
+        {
+            Debug.LogError("CountdownSprites 배열에 4개의 스프라이트(3,2,1,Start)가 필요합니다.");
+            yield break;
+        }
+
+        CountdownImage.sprite = CountdownSprites[0]; // 3
+        AudioManager.Instance.SFX.PlayGameStart();
         yield return new WaitForSecondsRealtime(1f);
-        SecondsText.text = "2"; // 2초 표시
+
+        CountdownImage.sprite = CountdownSprites[1]; // 2
         yield return new WaitForSecondsRealtime(1f);
-        SecondsText.text = "1"; // 1초 표시
+
+        CountdownImage.sprite = CountdownSprites[2]; // 1
         yield return new WaitForSecondsRealtime(1f);
-        SecondsText.text = "Start!"; // 시작 표시
+
+        CountdownImage.sprite = CountdownSprites[3]; // Start!
         yield return new WaitForSecondsRealtime(1f);
-        SecondsText.gameObject.SetActive(false); // 텍스트 숨김
-        TouchBlockPanel.gameObject.SetActive(false); //터치 막는 패널 비활성화
-        Time.timeScale = 1f; // 게임 시간 재개
+
+        CountdownImage.gameObject.SetActive(false);
+        TouchBlockPanel.gameObject.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
